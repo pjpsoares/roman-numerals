f76bbcb4b886e1b09fc149709eb9f5cdc183d3d6
Paulo Soares <paulo@yieldify.com>
2016-09-05 19:52:01 +0100

    Add validation for out of range numbers on initialization

--- src/roman-number.js
+++ src/roman-number.js
@@ -1,6 +1,15 @@
+const MIN_RANGE = 1;
+const MAX_RANGE = 3999;
+
 function RomanNumber(value) {
   if (value === undefined || value === null || value === '') {
     throw new Error('value required');
+  }
+
+  if (Number.isInteger(value)) {
+    if (value < MIN_RANGE || value > MAX_RANGE) {
+      throw new Error('invalid range');
+    }
   }
 }
 
--- test/roman-number.spec.js
+++ test/roman-number.spec.js
@@ -1,7 +1,7 @@
 const RomanNumber = require('../src/roman-number');
 const expect = require('chai').expect;
 
-function assertValueRequired(value) {
+function assertValueRequiredError(value) {
   describe(`when we instantiate with ${value}`, () => {
     it('should throw a value required error', () => {
       expect(() => { new RomanNumber(value); })
@@ -10,10 +10,21 @@ function assertValueRequired(value) {
   });
 }
 
+function assertInvalidRangeError(value) {
+  describe(`when we instantiate with ${value}`, () => {
+    it('should throw an invalid range error', () => {
+      expect(() => { new RomanNumber(value); })
+        .to.throw('invalid range');
+    });
+  });
+}
+
 describe('Roman Number', () => {
 
-  assertValueRequired(null);
-  assertValueRequired();
-  assertValueRequired('');
-
+  assertValueRequiredError(null);
+  assertValueRequiredError();
+  assertValueRequiredError('');
+  assertInvalidRangeError(0);
+  assertInvalidRangeError(4000);
+  assertInvalidRangeError(10000);
 });
